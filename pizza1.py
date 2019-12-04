5a6587257a7a47aeef888b17128c48742b8af0c0
ValentinaLV <tina.lysenok@gmail.com>
2019-12-04 14:02:45 +0200

    hw13_py_creating_pizza

--- pizza1.py
+++ pizza1.py
@@ -1,10 +1,57 @@
+"""
+module for making delicious pizza
+"""
+
+
 class Pizza:
-    pass
+    """
+    Class that creates delicious pizza.
+    """
+
+    def __init__(self):
+        self.ingrts = ['ham', 'tomatoes', 'cheese']
+
+    def add_ingredient(self, ingrt):
+        """
+        Add ingredient in recipe for making pizza.
+        :param ingrt:
+        :return:
+        """
+        return self.ingrts.append(ingrt)
+
+    def replace_ingredient(self, prev_ingrt, new_ingrt):
+        """
+        Replace ingredient in recipe for making pizza.
+        :param prev_ingrt:
+        :param new_ingrt:
+        :return:
+        """
+        return [new_ingrt if ingrt == prev_ingrt else ingrt for ingrt in self.ingrts]
+
+    def make_recipe(self):
+        """
+        Return recipe for making pizza.
+        :return: str
+        """
+        add_ingrts = ['Add ' + ingrt for ingrt in self.ingrts]
+        return 'Make dough\n' + '\n'.join(add_ingrts) + '\nBake'
 
 
 class PopcornPizza(Pizza):
-    pass
+    """
+    Class that creates delicious popcorn pizza.
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.ingrts = ['ham', 'popcorn', 'cheese']
 
 
-class ChoclatePizza(Pizza):
-    pass
+class ChocolatePizza(Pizza):
+    """
+        Class that creates chocolate pizza.
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.ingrts = ['fish', 'popcorn', 'chocolate']
