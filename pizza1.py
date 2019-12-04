f727acbc9fdec1f01690ea58178cc1e4f7e90910
d1makov <makovskyj.danylo@gmail.com>
2019-12-04 17:33:13 +0200

    fin

--- pizza1.py
+++ pizza1.py
@@ -1,10 +1,98 @@
+"""
+docstring
+"""
+
+
 class Pizza:
-    pass
+    """
+    pizza recipe
+    """
+
+    def make_recipe(self):
+        """
+
+        :return:
+        """
+        return ("Make dough\n" +
+                self.ing_one() + "\n" +
+                self.ing_two() + "\n" +
+                self.ing_three() + "\n" +
+                "Bake")
+
+    @staticmethod
+    def ing_one():
+        """
+
+        :return:
+        """
+        return "Add ham"
+
+    @staticmethod
+    def ing_two():
+        """
+
+        :return:
+        """
+        return "Add tomatoes"
+
+    @staticmethod
+    def ing_three():
+        """
+
+        :return:
+        """
+        return "Add cheese"
 
 
 class PopcornPizza(Pizza):
-    pass
+    """
+    popcornpizze recipe
+    """
+
+    def ing_one(self):
+        """
+
+        :return:
+        """
+        return "Add ham"
+
+    def ing_two(self):
+        """
+
+        :return:
+        """
+        return "Add popcorn"
+
+    def ing_three(self):
+        """
+
+        :return:
+        """
+        return "Add cheese"
 
 
 class ChoclatePizza(Pizza):
-    pass
+    """
+    choclatepizze recipe
+    """
+
+    def ing_one(self):
+        """
+
+        :return:
+        """
+        return "Add fish"
+
+    def ing_two(self):
+        """
+
+        :return:
+        """
+        return "Add popcorn"
+
+    def ing_three(self):
+        """
+
+        :return:
+        """
+        return "Add chocolate"
