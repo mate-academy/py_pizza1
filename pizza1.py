19281b8e6809c9e8e7d8e7df1ea3d6af6aa7112c
Dima <dima.sapsay@gmail.com>
2019-12-04 16:30:14 +0200

    first commit

--- pizza1.py
+++ pizza1.py
@@ -1,10 +1,36 @@
+"""
+Create a class to generate pizza recipe.
+Provide the ability to change ingredients in subclasses.
+"""
+
+
 class Pizza:
-    pass
+    """Pizza class"""
+    def __init__(self):
+        self.ingredients = ['ham', 'tomatoes', 'cheese']
+
+    def make_recipe(self):
+        """Returns pizza recipe"""
+        strg = "Make dough\n"
+        for i in self.ingredients:
+            strg += "Add {}\n".format(i)
+        return strg + "Bake"
+
+    def get_ingredients(self):
+        """Returns pizza ingredients"""
+        return self.ingredients
 
 
 class PopcornPizza(Pizza):
-    pass
+    """PopcornPizza subclass Pizza"""
+    def __init__(self):
+        super().__init__()
+        self.ingredients = ['ham', 'popcorn', 'cheese']
+
 
 
 class ChoclatePizza(Pizza):
-    pass
+    """ChoclatePizza subclass Pizza"""
+    def __init__(self):
+        super().__init__()
+        self.ingredients = ['fish', 'popcorn', 'chocolate']
