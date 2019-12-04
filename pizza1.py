fe10545acfd4c2917d1d59301f5a06f30b11e7d7
DukhDmytro <cowboybebop4991@gmail.com>
2019-12-04 16:24:51 +0200

    1st commit

--- pizza1.py
+++ pizza1.py
@@ -1,10 +1,32 @@
+"""modules"""
+
+
 class Pizza:
-    pass
+    """class to represent usual pizza"""
+    def __init__(self):
+        self.ingredients = ["ham", "tomatoes", "cheese"]
+
+    def make_recipe(self):
+        """return recipe for pizza"""
+        recipe = "Make dough\n"
+        for ingredient in self.ingredients:
+            recipe += f"Add {ingredient}\n"
+        return recipe + "Bake"
+
+    def get_ingredients(self):
+        """return list of ingredients"""
+        return self.ingredients
 
 
 class PopcornPizza(Pizza):
-    pass
+    """class to represent special kind of pizza with popcorn"""
+    def __init__(self):
+        super().__init__()
+        self.ingredients = ["ham", "popcorn", "cheese"]
 
 
-class ChoclatePizza(Pizza):
-    pass
+class ChocolatePizza(Pizza):
+    """class to represent special kind of pizza with chocolate"""
+    def __init__(self):
+        super().__init__()
+        self.ingredients = ["fish", "popcorn", "chocolate"]
