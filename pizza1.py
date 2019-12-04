b09ed59a37f465108c5eb4d8674368bc95ce4056
logotip <logotip123@yahoo.com>
2019-12-04 22:06:13 +0200

    v 1.1

--- pizza1.py
+++ pizza1.py
@@ -6,20 +6,21 @@ class Pizza:
     def __init__(self):
         self._ingredients = ["ham", "tomatoes", "cheese"]
 
+    def show_ingredients(self):
+        """
+        :return: list with ingredients
+        """
+        return self._ingredients
+
     def make_recipe(self):
         """
         :return: pizza recipe
         """
-        recipe = GeneratePizzaRecipe(self._ingredients)
-        return recipe.make_recipe()
-
-    def add_ingredient(self, ingredient):
-        """
-        :param ingredient: add argument to ingredients
-        :return: None
-        """
-        self._ingredients.append(ingredient)
-
+        result = "Make dough\n"
+        for i in self._ingredients:
+            result += f"Add {i}\n"
+        result += "Bake"
+        return result
 
 
 class PopcornPizza(Pizza):
@@ -34,21 +35,3 @@ class ChoclatePizza(Pizza):
     def __init__(self):
         super().__init__()
         self._ingredients = ["fish", "popcorn", "chocolate"]
-
-class GeneratePizzaRecipe:
-    """Class for generate pizza recipe"""
-    def __init__(self, ingredients):
-        self._ingredients = ingredients
-
-    def __str__(self):
-        return self._ingredients
-
-    def make_recipe(self):
-        """
-        :return: pizza recipe
-        """
-        result = "Make dough\n"
-        for i in self._ingredients:
-            result += f"Add {i}\n"
-        result += "Bake"
-        return result
