2d1d599253943141f59f35d71ffef7abfecf55b2
YuPo13 <yuliya.povzlo@gmail.com>
2019-12-05 02:25:32 +0200

    After you've explained the core idea of the task the realization was almost easy.

--- pizza1.py
+++ pizza1.py
@@ -1,10 +1,46 @@
+"""This program depicts process of pizza recipes creation"""
+
+
 class Pizza:
-    pass
+    """This is parent class that features unique list of ingredients
+    and standardised recipe creation procedure"""
+    def __init__(self):
+        """Initiate the list of ingredients"""
+        self.list_of_ingredients = ["ham", "tomatoes", "cheese"]
+
+    @staticmethod
+    def begin():
+        """May be put into return of make_recipe function directly"""
+        return "Make dough\n"
+
+    def add_smth(self):
+        """Create the product mix"""
+        products_mix = ""
+        for ingredient in self.list_of_ingredients:
+            products_mix += "Add " + ingredient + "\n"
+        return products_mix
+
+    @staticmethod
+    def finish():
+        """May be put into return of make_recipe function directly"""
+        return "Bake"
+
+    def make_recipe(self):
+        """Finalize the recipe into one piece of text"""
+        return Pizza.begin() + self.add_smth() + Pizza.finish()
 
 
 class PopcornPizza(Pizza):
-    pass
+    """This is subclass inheriting all methods of parent class"""
+    def __init__(self):
+        """Distinguish the unique ingredients list"""
+        super().__init__(self)
+        self.list_of_ingredients = ["ham", "popcorn", "cheese"]
 
 
-class ChoclatePizza(Pizza):
-    pass
+class ChocolatePizza(Pizza):
+    """This is subclass inheriting all methods of parent class"""
+    def __init__(self):
+        """Distinguish the unique ingredients list"""
+        super().__init__(self)
+        self.list_of_ingredients = ["fish", "popcorn", "chocolate"]
