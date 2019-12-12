baab5ec07ce2f89906f58d4d372a04142120e78e
alexander <earthhuman41@gmail.com>
2019-12-12 16:29:43 +0200

    pizza

--- pizza1.py
+++ pizza1.py
@@ -1,10 +1,59 @@
+"""
+Pizza
+PopcornPizza
+ChocolatePizza
+"""
+
+
 class Pizza:
-    pass
+    """
+    pizza
+    """
+    def __init__(self):
+        super().__init__()
+        self._ingridients = [
+            'ham',
+            'tomatoes',
+            'cheese',
+        ]
+
+    def add_ingredient(self, value):
+        """
+        add_ingredient
+        """
+        self._ingridients.append(value)
+
+    def make_recipe(self):
+        """
+        make_reciepe
+        """
+        pizza = "Make dough\nAdd """
+        pizza += "\nAdd ".join(self._ingridients)
+        pizza += "\nBake"
+        return pizza
 
 
 class PopcornPizza(Pizza):
-    pass
+    """
+    popcornPizza
+    """
+    def __init__(self):
+        super().__init__()
+        self._ingridients = [
+            'ham',
+            'popcorn',
+            'cheese',
+        ]
 
 
 class ChoclatePizza(Pizza):
-    pass
+    """
+    chocolatePizza
+    """
+    def __init__(self):
+        super().__init__()
+        self._ingridients = [
+            'fish',
+            'popcorn',
+            'chocolate',
+        ]
